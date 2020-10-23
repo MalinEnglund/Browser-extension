f441b2111b94a7b64cbeca9e2d1eaefff01701e5
MalinEnglund <fyrklover93@gmail.com>
2020-10-23 13:24:17 +0200

    Update detection.js

--- background/detection.js
+++ background/detection.js
@@ -34,23 +34,11 @@ function detectWarning(details) {
 
   const error_codes_causing_browser_warnings = ['net::ERR_CERT_DATE_INVALID','net::ERR_CERT_AUTHORITY_INVALID',
   'net::ERR_CERT_COMMON_NAME_INVALID','net::ERR_CERT_WEAK_SIGNATURE_ALGORITHM',
-  'net::ERR_CERTIFICATE_TRANSPARENCY_REQUIRED', 'net::ERR_CERT_SYMANTEC_LEGACY'];
+  'net::ERR_CERTIFICATE_TRANSPARENCY_REQUIRED', 'net::ERR_CERT_SYMANTEC_LEGACY','net::ERR_BLOCKED_BY_CLIENT'];
 
-  if((details.error === 'net::ERR_BLOCKED_BY_CLIENT') && (details.frameId === 0)){
-
+  if((details.error === error_codes_causing_browser_warnings.includes(details.error)) && (details.frameId === 0)){
       dataGathering(data, details.url, details.error)
-
       detectAnswer(details.url, details.tabId, data)
-
-  }else if(error_codes_causing_browser_warnings.includes(details.error) && (details.frameId === 0)){
-
-    //TODO:solve captive portal false positives
-    //solution for firefox: captivePortal API
-
-    dataGathering(data, details.url, details.error)
-
-    detectAnswer(details.url, details.tabId, data)
-
   }
 
 }
